4579c9c6a80457c704fa38001302c65cd874a054
Vinit <singhvinit435@gmail.com>
2026-01-04 20:45:06 +0530

    implemented authentication

---
+++ constant/userAvatar.ts
@@ -0,0 +1,4 @@
+export default {
+  maleAvatar: "https://img.freepik.com/premium-vector/man-avatar-profile-picture-isolated-background-avatar-profile-picture-man_1293239-4841.jpg",
+  femaleAvatar: "https://static.vecteezy.com/system/resources/thumbnails/020/271/547/small_2x/portrait-of-a-beautiful-asian-woman-full-face-portrait-in-flat-style-avatar-female-diversity-free-vector.jpg",
+}
